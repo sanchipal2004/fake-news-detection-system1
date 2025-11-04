8d7cc00a4d77770b89aaa1e674967e9427552ce7
sanchipal2004 <sanchipal2004@gmail.com>
2025-11-04 15:18:26 +0530

    Update Footer.jsx

--- fnd/src/assets/components/Footer.jsx
+++ fnd/src/assets/components/Footer.jsx
@@ -10,10 +10,11 @@ const Footer = () => {
           <div className="flex max-w-[960px] flex-1 flex-col">
             <footer className="flex flex-col gap-6 px-5 py-10 text-center @container">
               <div className="flex flex-wrap items-center justify-center gap-6 @[480px]:flex-row @[480px]:justify-around">
-                <a className="text-white text-base font-normal leading-normal min-w-40" href="#">About Us</a>
-                <a className="text-white text-base font-normal leading-normal min-w-40" href="#">Contact</a>
+                <a className="text-white text-base font-normal leading-normal min-w-40" href="/about">About Us</a>
+                <h1 className="text-white text-base font-normal leading-normal min-w-40">Contact</h1>
+                <p></p>
                 <a className="text-white text-base font-normal leading-normal min-w-40" href="#">Privacy Policy</a>
-                <a className="text-white text-base font-normal leading-normal min-w-40" href="#">Terms of Service</a>
+                <a className="text-white text-base font-normal leading-normal min-w-40" href="/termofservice">Terms of Service</a>
               </div>
               <div className="flex flex-wrap justify-center gap-4">
                 <a href="#">
